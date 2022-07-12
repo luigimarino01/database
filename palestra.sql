314bb5fba48752422ac131363b03aec248bb8b15
luigimarino01 <luigimarinodev@gmail.com>
2022-07-12 17:26:27 +0200

    ok

--- palestra.sql
+++ palestra.sql
@@ -14,7 +14,7 @@ CREATE TABLE Abbonamento (
    NomePalestra varchar(50) NOT NULL,
    PrezzoAbbonamento float NOT NULL,
    FOREIGN KEY(NomePalestra) REFERENCES Sede(NomePalestra),
-   PRIMARY KEY(TipoAbbonamento, NomePalestra)
+   Constraint PKAbbonamento PRIMARY KEY(TipoAbbonamento, NomePalestra)
 );
 
 CREATE TABLE Corso(
@@ -25,6 +25,8 @@ CREATE TABLE Corso(
     FOREIGN KEY(NomePalestra) REFERENCES Sede(NomePalestra),
     PRIMARY KEY(NomeCorso, NomePalestra)
 );
+
+
 
 CREATE TABLE Servizio(
     NomeServizio varchar(50) NOT NULL,
@@ -48,7 +50,7 @@ CREATE TABLE Vende(
     DataVenditaProdotto date NOT NULL,
     FOREIGN KEY(NomePalestra) REFERENCES Sede(NomePalestra),
     FOREIGN KEY(CodiceProdotto) REFERENCES Prodotto(CodiceProdotto),
-    PRIMARY KEY(NomePalestra,CodiceProdotto)
+    PRIMARY KEY(NomePalestra,CodiceProdotto) 
 );
 
 CREATE TABLE Persona(
@@ -64,6 +66,14 @@ CREATE TABLE Proprietario(
     DataContrattoAcquisto date NOT NULL,
     FOREIGN KEY(CFPersona) REFERENCES Persona(CFPersona),
     PRIMARY KEY(CFPersona)
+);
+
+CREATE TABLE Appartiene(
+     CFPersona varchar(16) NOT NULL,
+     NomePalestra varchar(50) NOT NULL,
+     FOREIGN KEY(CFPersona) REFERENCES Proprietario(CFPersona),
+     FOREIGN KEY(NomePalestra) REFERENCES Sede(NomePalestra),
+     PRIMARY KEY(CFPersona,NomePalestra)
 );
 
 CREATE TABLE Dipendente(
@@ -89,7 +99,8 @@ CREATE TABLE Istruttore(
 CREATE TABLE Iscritto(
     NumeroTesseraIscritto varchar(50),
     CFPersona varchar(16) NOT NULL,
-    FOREIGN KEY(CFPersona) REFERENCES Persona(CFPersona)
+    FOREIGN KEY(CFPersona) REFERENCES Dipendente(CFPersona),
+    PRIMARY KEY(CFPersona)
 );
 
 CREATE TABLE Acquista(
@@ -115,12 +126,11 @@ CREATE TABLE Sottoscrive(
     DataSottoiscrizioneAbbonamento date NOT NULL,
     DataScadenzaAbbonamento date NOT NULL,
     CFPersona varchar(16) NOT NULL,
-    TipoAbbonamento varchar(50) NOT NULL,
-    NomePalestra varchar(50) NOT NULL,
+    TipoAbbonamento varchar(16) NOT NULL,
+    NomePalestra varchar (50) NOT NULL,
     FOREIGN KEY(CFPersona) REFERENCES Iscritto(CFPersona),
-    FOREIGN KEY(TipoAbbonamento) REFERENCES Abbonamento(TipoAbbonamento),
-    FOREIGN KEY(NomePalestra) REFERENCES Abbonamento(NomePalestra)
-
+    FOREIGN KEY(TipoAbbonamento,NomePalestra) REFERENCES Abbonamento(TipoAbbonamento,NomePalestra),
+    PRIMARY KEY(CFPersona,NomePalestra,TipoAbbonamento,DataSottoiscrizioneAbbonamento)
 );
 
 CREATE TABLE Conduce(
@@ -128,7 +138,56 @@ CREATE TABLE Conduce(
     OraConduzione int NOT NULL,
     CFPersona varchar(16) NOT NULL,
     NomeCorso varchar(50) NOT NULL,
+    NomePalestra varchar (50) NOT NULL,
     FOREIGN KEY(CFPersona) REFERENCES Istruttore(CFPersona),
-    FOREIGN KEY(NomeCorso) REFERENCES Corso(NomeCorso),
+    FOREIGN KEY(NomeCorso,NomePalestra) REFERENCES Corso(NomeCorso,NomePalestra),
     PRIMARY KEY(GiornoConduzione,OraConduzione)
+);
+
+CREATE TABLE Accede(
+    DataAccesso date NOT NULL,
+    CostoServizio float NOT NULL,
+    CFPersona varchar(16) NOT NULL,
+    NomeServizio varchar(50) NOT NULL,
+    NomePalestra varchar(50) NOT NULL,
+    FOREIGN KEY(CFPersona) REFERENCES Iscritto(CFPersona),
+    FOREIGN KEY(NomeServizio,NomePalestra) REFERENCES Servizio(NomeServizio,NomePalestra),
+    PRIMARY KEY(DataAccesso,CFPersona,NomeServizio)
+);
+
+CREATE TABLE Prenotazione(
+    CodicePrenotazione varchar(15) NOT NULL,
+    DataPrenotazione date NOT NULL,
+    CFPersona varchar(16) NOT NULL,
+    NomeCorso varchar(50) NOT NULL,
+    NomePalestra varchar(50) NOT NULL,
+    FOREIGN KEY(NomeCorso,NomePalestra) REFERENCES Corso(NomeCorso,NomePalestra),
+    FOREIGN KEY(CFPersona) REFERENCES Iscritto(CFPersona),
+    PRIMARY KEY(CodicePrenotazione)
+);
+
+CREATE TABLE SchedaAllenamento(
+    CodiceSchedaAllenamento varchar(15) NOT NULL,
+    ScadenzaSchedaAllenamento date NOT NULL,
+    CFPersonaIscritto varchar(16) NOT NULL,
+    CFPersonaIstruttore varchar(16) NOT NULL,
+    FOREIGN KEY(CFPersonaIscritto) REFERENCES Iscritto(CFPersona),
+    FOREIGN KEY(CFPersonaIstruttore) REFERENCES Istruttore(CFPersona),
+    PRIMARY KEY(CodiceSchedaAllenamento)
+);
+
+CREATE TABLE Esercizio(
+    NomeEsercizio varchar(50) NOT NULL,
+    GruppoMuscolare varchar(30) NOT NULL,
+    PRIMARY KEY(NomeEsercizio)
+);
+
+CREATE TABLE Contiene(
+    Serie int NOT NULL,
+    Ripetizioni int NOT NULL,
+    NomeEsercizio varchar(50) NOT NULL,
+    CodiceSchedaAllenamento varchar(15) NOT NULL,
+    FOREIGN KEY(NomeEsercizio) REFERENCES Esercizio(NomeEsercizio),
+    FOREIGN KEY(CodiceSchedaAllenamento) REFERENCES SchedaAllenamento(CodiceSchedaAllenamento),
+    PRIMARY KEY(NomeEsercizio, CodiceSchedaAllenamento)
 );
